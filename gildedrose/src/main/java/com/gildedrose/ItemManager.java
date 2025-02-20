a2a0afa959e192531598e5a72ac0b25500813514
Alex <alexius.lzc@gmail.com>
2025-02-20 21:43:16 +0800

    after cleanup

--- gildedrose/src/main/java/com/gildedrose/ItemManager.java
+++ gildedrose/src/main/java/com/gildedrose/ItemManager.java
@@ -16,29 +16,4 @@ public class ItemManager {
             return new GenericItem(item);
         }
     }
-
-    /* attempted with switch case but java21 preview features cant be compiled
-    switch (item.name) {
-        case String s when s.contains("Sulfuras") -> {
-            //return new EnhancedItem1(item, false, 0, false);
-            return new Sulfras(item);
-        }
-        case "Aged Brei" -> {
-            //return new EnhancedItem1(item, false, 1, false);
-            return new AgedBrei(item);
-        }
-        case String s when s.contains("Backstage passes") -> {
-            //return new EnhancedItem1(item, false, 1, true);
-            return new BackStagePass(item);
-        }
-        case String s when s.contains("Conjured") -> {
-            //return new EnhancedItem1(item, true, 2, false);
-            return new ConjuredItem(item);
-        }
-        default -> {
-            //return new EnhancedItem1(item, true, 1, false);
-            return new GenericItem(item);
-        }
-    }
-     */
 }
---
+++ gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,28 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    //@Test
+    //void foo() {
+    //    Item[] items = new Item[] { new Item("foo", 0, 0) };
+    //    GildedRoseOriginal app = new GildedRoseOriginal(items);
+    //    app.updateQuality();
+    //    assertEquals("fixme", app.items[0].name);
+    //}
+    @Test
+    void shouldUpdateQualityWhenCalled() {
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+        Item[] items = new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+
+        Item expectedItem = new Item("+5 Dexterity Vest", 9, 19);
+        assertThat(item).isEqualToComparingFieldByField(expectedItem);
+    }
+
+}
